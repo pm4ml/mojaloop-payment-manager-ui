3abe7fafbbb1beed10513bacf5d8bef9b1ff99d5
v3-Panwar-Ujjwal <v3.panwarujjwal@gmail.com>
2025-01-20 16:21:19 +0530

    chore: update setupStyle function

--- src/utils/setupStyles.ts
+++ src/utils/setupStyles.ts
@@ -3,6 +3,8 @@ import { AppUiConfig } from '../App/types';
 const setupStyles = (uiConfigurations: AppUiConfig) => {
   setPrimaryColor(uiConfigurations.primaryColor);
   setSecondaryColor(uiConfigurations.secondaryColor);
+  setAccentColor(uiConfigurations.accentColor);
+  setNavbarColor(uiConfigurations.secondaryColor);
 };
 
 const setPrimaryColor = (primaryColor: string) => {
@@ -12,4 +14,13 @@ const setPrimaryColor = (primaryColor: string) => {
 const setSecondaryColor = (secondaryColor: string) => {
   document.documentElement.style.setProperty('--secondary-color', secondaryColor);
 };
+
+const setAccentColor = (accentColor: string) => {
+  document.documentElement.style.setProperty('--accent-color', accentColor);
+};
+
+const setNavbarColor = (navbarColor: string) => {
+  document.documentElement.style.setProperty('--navbar-color', navbarColor);
+};
+
 export default setupStyles;
