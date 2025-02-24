3c83d09acdc16552f176ac40a62976afcdd41400
nguni52 <nguni52@gmail.com>
2025-02-24 19:35:45 +0200

    Updater dfault nix

--- ui-tests/default.nix
+++ ui-tests/default.nix
@@ -1,16 +1,16 @@
 # Want to update dependencies? Go to https://github.com/NixOS/nixpkgs/tree/nixpkgs-unstable and
 # replace the commit ref in the following line with the one you're interested in (probably the
 # latest).
-{ nixpkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/08ef0f28e3a41424b92ba1d203de64257a9fca6a.tar.gz) { config = { allowUnfree = true; }; } }:
+{ nixpkgs ? import (fetchTarball https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz) { config = { allowUnfree = true; }; } }:
 
 let
   k3d = nixpkgs.stdenv.mkDerivation rec {
-    version = "4.4.1";
+    version = "5.4.6";  # Updated version
     pname = "k3d";
 
     src = builtins.fetchurl {
-      url = "https://github.com/rancher/k3d/releases/download/v4.4.1/k3d-linux-amd64";
-      sha256 = "1bjmyhf0zbi6lfq71h6vazmlkxg0b46wky5vqv1dqbkr2bdr2s24";
+      url = "https://github.com/rancher/k3d/releases/download/v5.4.6/k3d-linux-amd64";  # Updated URL
+      sha256 = "0v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z";  # Replace with the correct SHA256
     };
 
     dontUnpack = true;
@@ -25,11 +25,11 @@ let
   };
 
   skaffold = nixpkgs.stdenv.mkDerivation rec {
-    version = "1.28.0";
+    version = "1.35.0";  # Updated version
     pname = "skaffold";
     src = builtins.fetchurl {
-      url = "https://github.com/GoogleContainerTools/skaffold/releases/download/v1.28.0/skaffold-linux-amd64";
-      sha256 = "1aiggw0b8655mzzf57xv079vzgfj4k3xwlr7l48y2pvbzy46f0mg";
+      url = "https://github.com/GoogleContainerTools/skaffold/releases/download/v1.35.0/skaffold-linux-amd64";  # Updated URL
+      sha256 = "0v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z6v8x4k5z";  # Replace with the correct SHA256
     };
     dontUnpack = true;
     installPhase = ''
