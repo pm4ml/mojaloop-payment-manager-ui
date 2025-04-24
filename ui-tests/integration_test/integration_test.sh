bc20e26a38254c405a9a262abe3abb4f79e8dedd
Nguni Phakela <nguni52@gmail.com>
2025-04-24 10:47:32 +0200

    Update script

--- ui-tests/integration_test/integration_test.sh
+++ ui-tests/integration_test/integration_test.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-# # Exit immediately if a command exits with a non-zero status
+# Exit immediately if a command exits with a non-zero status
 set -e
 
 # # Change to the directory containing this script
 cd "$(dirname "$0")"
 
-# # # Install test dependencies
+# Install test dependencies
 cd ../tests
 npm ci
 
-# # # Run tests
+# Run tests
 PAYMENT_MANAGER_ENDPOINT="http://127.0.0.1:3000" npm run test:headless
 
-# # # Archive test report
+# Archive test report
 cp report.html /path/to/artifacts/test-report.html
