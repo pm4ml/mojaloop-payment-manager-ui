1c9c26f99fc02c564dbf4ceae82af6d6fd6ca3e8
Nguni Phakela <nguni52@gmail.com>
2025-04-28 19:58:58 +0200

    Remove startup of UI.

--- ui-tests/integration_test/integration_test.sh
+++ ui-tests/integration_test/integration_test.sh
@@ -10,25 +10,8 @@ cd "$(dirname "$0")"
 cd ../tests
 npm ci
 
-# Add on prem deploy before we run the actual integration tests
-# checkout payment docker compose
-
-# Start the server in the background
-npm run start &
-SERVER_PID=$!
-
-# Wait for the server to be ready
-echo "Waiting for server to start..."
-until curl -s http://127.0.0.1:3000 > /dev/null; do
-    sleep 1
-done
-echo "Server is up and running!"
-
 # Run tests
 PM4ML_ENDPOINT="http://127.0.0.1:8083" npm run test:headless
-
-# Clean up - kill the background server process
-kill $SERVER_PID
 
 # Archive test report
 mkdir -p /tmp/test-results
