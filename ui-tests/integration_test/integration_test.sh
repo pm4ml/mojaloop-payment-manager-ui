477f9a5ed516beaee16f523f6428917268386d06
Nguni Phakela <nguni52@gmail.com>
2025-04-28 20:22:17 +0200

    even if integration tests fail, copy the html file so we can see what the errors were

--- ui-tests/integration_test/integration_test.sh
+++ ui-tests/integration_test/integration_test.sh
@@ -3,16 +3,16 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# # Change to the directory containing this script
+# Change to the directory containing this script
 cd "$(dirname "$0")"
 
 # Install test dependencies
 cd ../tests
 npm ci
+mkdir -p /tmp/test-results
 
 # Run tests
-PM4ML_ENDPOINT="http://127.0.0.1:8083" npm run test:headless
+PM4ML_ENDPOINT="http://127.0.0.1:8083" npm run test:headless || true
 
 # Archive test report
-mkdir -p /tmp/test-results
-cp report.html /tmp/test-results/test-report.html
+cp report.html /tmp/test-results/test-report.html || true
